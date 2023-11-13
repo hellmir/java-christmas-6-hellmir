0fcfd09ec19f4b3c7c9d689c75cc144bc7fe0d55
Sung Hyobin <hellmir610@gmail.com>
2023-11-14 07:13:11 +0900

    test: Add test of function that validate order menus

--- src/test/java/christmas/service/EventPlannerServiceTest.java
+++ src/test/java/christmas/service/EventPlannerServiceTest.java
@@ -10,8 +10,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.util.ArrayList;
 import java.util.List;
 
-import static christmas.message.ErrorMessage.ERROR_MESSAGE_HEAD;
-import static christmas.message.ErrorMessage.INVALID_DATE_EXCEPTION;
+import static christmas.message.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -64,6 +63,78 @@ class EventPlannerServiceTest {
                 .containsExactly(menuQuantities.get(0), menuQuantities.get(1), menuQuantities.get(2));
 
         checkMenuNames(orderMenuDtos, koreanMenuNames);
+    }
+
+    @DisplayName("메뉴판에 없는 메뉴를 전송하면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "'케이크-3,양송이수프-2,제로콜라-3'", "'샴페인-2,파스타-5,타파스-1'", "'아이스크림-1,수프-3,와인-7'"
+    })
+    void generateOrderWithNotExistentMenu(String orderInput) {
+        // given, when, then
+        assertThatThrownBy(() -> eventPlannerService.generateOrder(orderInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_HEAD + INVALID_ORDER_EXCEPTION);
+    }
+
+    @DisplayName("전송된 메뉴 개수가 1보다 적으면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "'초코케이크-0,양송이수프-2,제로콜라-3'", "'샴페인-2,해산물파스타-0,타파스-1'", "'아이스크림-0,바비큐립-3,레드와인-0'"
+    })
+    void generateOrderWithUnderOneQuantity(String orderInput) {
+        // given, when, then
+        assertThatThrownBy(() -> eventPlannerService.generateOrder(orderInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_HEAD + INVALID_ORDER_EXCEPTION);
+    }
+
+    @DisplayName("전송된 메뉴 형식이 예시와 다르면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "'초코케이크--3,양송이수프-2,제로콜라-3'", "'샴페인-2,해산물파스타5,타파스-1'", "'아이스크림-1,바비큐립-3,레드와인-07'"
+    })
+    void generateOrderWithInvalidFormat(String orderInput) {
+        // given, when, then
+        assertThatThrownBy(() -> eventPlannerService.generateOrder(orderInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_HEAD + INVALID_ORDER_EXCEPTION);
+    }
+
+    @DisplayName("중복된 메뉴를 전송하면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "'초코케이크-3,양송이수프-2,초코케이크-3'", "'해산물파스타-2,샴페인-5,샴페인-1'", "'레드와인-1,레드와인-3,레드와인-7'"
+    })
+    void generateOrderWithDuplicateMenu(String orderInput) {
+        // given, when, then
+        assertThatThrownBy(() -> eventPlannerService.generateOrder(orderInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_HEAD + INVALID_ORDER_EXCEPTION);
+    }
+
+    @DisplayName("음료만 포함된 메뉴를 전송하면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "'제로콜라-3,샴페인-2,레드와인-3'", "'샴페인-2,레드와인-5,제로콜라-1'", "'레드와인-1,샴페인-3,제로콜라-7'"
+    })
+    void generateOrderWithOnlyBeverages(String orderInput) {
+        // given, when, then
+        assertThatThrownBy(() -> eventPlannerService.generateOrder(orderInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_HEAD + ONLY_BEVERAGES_ORDERED_EXCEPTION);
+    }
+
+    @DisplayName("20개를 초과하는 메뉴를 전송하면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "'초코케이크-3,양송이수프-2,제로콜라-16'", "'샴페인-27,해산물파스타-5,타파스-1'", "'아이스크림-1,바비큐립-52,레드와인-7'"
+    })
+    void generateOrderWithOverTwentyMenus(String orderInput) {
+        // given, when, then
+        assertThatThrownBy(() -> eventPlannerService.generateOrder(orderInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_HEAD + MENU_QUANTITY_EXCEEDED_EXCEPTION);
     }
 
     private void setOrderInformation(String orderInput, List<String> koreanMenuNames, List<Integer> menuQuantities) {
