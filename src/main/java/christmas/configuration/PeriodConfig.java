202a04772139bb63fa4219ecce8ebcc6fdc8b7bb
Sung Hyobin <hellmir610@gmail.com>
2023-11-14 07:37:03 +0900

    refactor: Remove getters in PeriodConfig class because it is unnecessary

--- src/main/java/christmas/configuration/PeriodConfig.java
+++ src/main/java/christmas/configuration/PeriodConfig.java
@@ -7,12 +7,4 @@ import static christmas.domain.Month.DECEMBER;
 public class PeriodConfig {
     public static final int year = 2023;
     public static final Month month = DECEMBER;
-
-    public int getYear() {
-        return year;
-    }
-
-    public Month getMonth() {
-        return month;
-    }
 }
