7602d2f5171d558730de46be38b72088a0d8b88f
Sung Hyobin <hellmir610@gmail.com>
2023-11-14 08:03:25 +0900

    style: Change constant names of PeriodConfig class into capital letters

--- src/main/java/christmas/configuration/PeriodConfig.java
+++ src/main/java/christmas/configuration/PeriodConfig.java
@@ -5,6 +5,6 @@ import christmas.domain.Month;
 import static christmas.domain.Month.DECEMBER;
 
 public class PeriodConfig {
-    public static final int year = 2023;
-    public static final Month month = DECEMBER;
+    public static final int YEAR = 2023;
+    public static final Month MONTH = DECEMBER;
 }
