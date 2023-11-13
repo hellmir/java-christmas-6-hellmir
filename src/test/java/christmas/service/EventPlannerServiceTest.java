62044e632848261cab278e4b47a7065afed28c6c
Sung Hyobin <hellmir610@gmail.com>
2023-11-14 07:12:35 +0900

    test: Add test of function that compute order menus

--- src/test/java/christmas/service/EventPlannerServiceTest.java
+++ src/test/java/christmas/service/EventPlannerServiceTest.java
@@ -1,13 +1,18 @@
 package christmas.service;
 
 import christmas.dto.ChosenDateDto;
+import christmas.dto.OrderDto;
+import christmas.dto.OrderMenuDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static christmas.message.ErrorMessage.ERROR_MESSAGE_HEAD;
 import static christmas.message.ErrorMessage.INVALID_DATE_EXCEPTION;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class EventPlannerServiceTest {
@@ -36,5 +41,43 @@ class EventPlannerServiceTest {
         assertThatThrownBy(() -> eventPlannerService.parseChosenDate(chosenDateInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_MESSAGE_HEAD + INVALID_DATE_EXCEPTION);
+    }
+
+    @DisplayName("주문 입력값을 통해 주문을 생성하고 주문 메뉴와 수량을 저장한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "'초코케이크-3,양송이수프-2,제로콜라-3'", "'샴페인-2,해산물파스타-5,타파스-1'", "'아이스크림-1,바비큐립-3,레드와인-7'"
+    })
+    void generateOrder(String orderInput) {
+        // given
+        List<String> koreanMenuNames = new ArrayList<>();
+        List<Integer> menuQuantities = new ArrayList<>();
+        setOrderInformation(orderInput, koreanMenuNames, menuQuantities);
+
+        // when
+        OrderDto orderDto = eventPlannerService.generateOrder(orderInput);
+
+        // then
+        List<OrderMenuDto> orderMenuDtos = orderDto.getOrderMenuDtos();
+
+        assertThat(orderDto.getOrderMenuDtos()).hasSize(3).extracting("menuQuantity")
+                .containsExactly(menuQuantities.get(0), menuQuantities.get(1), menuQuantities.get(2));
+
+        checkMenuNames(orderMenuDtos, koreanMenuNames);
+    }
+
+    private void setOrderInformation(String orderInput, List<String> koreanMenuNames, List<Integer> menuQuantities) {
+        String[] menus = orderInput.split(",");
+
+        for (String menu : menus) {
+            koreanMenuNames.add(menu.split("-")[0]);
+            menuQuantities.add(Integer.parseInt(menu.split("-")[1]));
+        }
+    }
+
+    private void checkMenuNames(List<OrderMenuDto> orderMenuDtos, List<String> koreanMenuNames) {
+        assertThat(orderMenuDtos.get(0).getMenuInformation().getKoreanName()).isEqualTo(koreanMenuNames.get(0));
+        assertThat(orderMenuDtos.get(1).getMenuInformation().getKoreanName()).isEqualTo(koreanMenuNames.get(1));
+        assertThat(orderMenuDtos.get(2).getMenuInformation().getKoreanName()).isEqualTo(koreanMenuNames.get(2));
     }
 }
