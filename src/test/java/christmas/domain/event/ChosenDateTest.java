df99d6ece743af1ad23aa7d2ef6b9454e76a07ae
Sung Hyobin <hellmir610@gmail.com>
2023-11-15 01:31:16 +0900

    refactor: Move package of test classes

--- src/test/java/christmas/domain/event/ChosenDateTest.java
+++ src/test/java/christmas/domain/event/ChosenDateTest.java
@@ -1,6 +1,5 @@
-package christmas.domain;
+package christmas.domain.event;
 
-import christmas.domain.event.ChosenDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
