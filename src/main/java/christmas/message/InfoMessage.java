d7e2c28461e5cbc83dbafa3897f918c232513e6c
Sung Hyobin <hellmir610@gmail.com>
2023-11-15 01:30:38 +0900

    style: Update changed constant name

--- src/main/java/christmas/message/InfoMessage.java
+++ src/main/java/christmas/message/InfoMessage.java
@@ -5,8 +5,8 @@ import christmas.domain.order.MenuInformation;
 
 public class InfoMessage {
     public static final String STARTING_INFO_MESSAGE = "안녕하세요! 우테코 식당 "
-            + PeriodConfig.month.getMonthNumber() + "월 이벤트 플래너입니다.";
-    public static final String DATE_CHOICE_MESSAGE = PeriodConfig.month.getMonthNumber()
+            + PeriodConfig.MONTH.getMonthNumber() + "월 이벤트 플래너입니다.";
+    public static final String DATE_CHOICE_MESSAGE = PeriodConfig.MONTH.getMonthNumber()
             + "월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static final String MENUS_CHOICE_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. "
             + MenuInformation.SEAFOOD_PASTA.getKoreanName() + "-2,"
