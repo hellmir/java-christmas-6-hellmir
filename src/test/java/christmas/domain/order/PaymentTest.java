7b95b1eb3d3cc315f2eb598318c0d07274b1294e
Sung Hyobin <hellmir610@gmail.com>
2023-11-15 08:11:30 +0900

    test: Add test of function that validate pament amount

--- src/test/java/christmas/domain/order/PaymentTest.java
+++ src/test/java/christmas/domain/order/PaymentTest.java
@@ -1,20 +1,33 @@
 package christmas.domain.order;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static christmas.message.ErrorMessage.ERROR_MESSAGE_HEAD;
+import static christmas.message.ErrorMessage.PAYMENT_AMOUNT_UNDER_ZERO_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PaymentTest {
-    private static int PAYMENT_AMOUNT = 10_000;
-
     @DisplayName("동일한 금액을 전송하면 동등한 Payment 인스턴스를 생성한다.")
-    @Test
-    void create() {
+    @ParameterizedTest
+    @CsvSource({"1", "10", "30_000"})
+    void create(int paymentAmount) {
         // given, when
-        Payment payment = new Payment(PAYMENT_AMOUNT);
+        Payment payment = new Payment(paymentAmount);
 
         // then
-        assertThat(payment).isEqualTo(new Payment(PAYMENT_AMOUNT));
+        assertThat(payment).isEqualTo(new Payment(paymentAmount));
+    }
+
+    @DisplayName("0원 미만의 금액을 전송하면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({"-1", "-10", "-30_000"})
+    void validate(int paymentAmount) {
+        // given, when, then
+        assertThatThrownBy(() -> new Payment(paymentAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_HEAD + PAYMENT_AMOUNT_UNDER_ZERO_EXCEPTION + paymentAmount);
     }
 }
