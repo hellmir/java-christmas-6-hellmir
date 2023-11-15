d35569dc4fe2ff7292748b42e617bdfb4c96fcbc
Sung Hyobin <hellmir610@gmail.com>
2023-11-15 13:04:48 +0900

    fix: Modify wrong logic of compute payment amount

--- src/main/java/christmas/domain/order/Payment.java
+++ src/main/java/christmas/domain/order/Payment.java
@@ -48,7 +48,7 @@ public class Payment {
     }
 
     public void updateDiscountAmount(ChristmasDiscount christmasDiscount) {
-        paymentAmount -= christmasDiscount.reducePaymentAmount(paymentAmount);
+        paymentAmount = christmasDiscount.reducePaymentAmount(paymentAmount);
         validate(paymentAmount);
     }
 
